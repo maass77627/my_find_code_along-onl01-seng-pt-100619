239f8bc7d72ae13d16ddfad796f2f9401f59fd5e
kelly r maass <kmaass77627@gmail.com>
2019-10-20 21:28:26 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,11 +2,12 @@
 
 def my_find(collection)
  i = 0 
- while i < 
- collection.length return collection[i] if 
-  yield(collection[i])
+ while i < collection.length 
+ return collection[i] if yield(collection[i])
  i = i + 1
  end 
 end
+
+
 
 
